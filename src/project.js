9eb93253520c868761e64e102c4188842d60507c
TomGhostSmith <TomGhostSmith@gmail.com>
2021-08-21 10:48:14 +0800

    Trial1
    Deleted the link to the wechat official account page, so it can be tested on the phone whether github is accessible or not.

--- src/project.js
+++ src/project.js
@@ -1901,6 +1901,7 @@ window.__require = function e(t, n, o) {
                 }, t.prototype.update = function(e) {
                     this.UpdateScoreLabel(e), this.lerpCtrl && this.lerpNumFunc(this.passlevelYQ), this.levelPanel.children[1].getComponent(cc.Label).string = s.default.Instance.GetLevel().toString()
                 }, t.prototype.adsButtonFunc = function() {
+
                     // window.location.href = "https://636f-codenav-8grj8px727565176-1256524210.tcb.qcloud.la/yupi_wechat.png"; // 关注作者微信
                 }, t.prototype.TestPasslevel = function() {
                     var e = this;
